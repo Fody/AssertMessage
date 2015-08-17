49e845fc5a1e8d378e41becb2671b640e8e55609
Rafał Jasica <rjasica@atsisa.com>
2015-08-17 09:24:27 +0200

    updated assembly version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("Fody.AssertMessage")]
 [assembly: AssemblyProduct("Fody.AssertMessage")]
-[assembly: AssemblyVersion("1.0.12")]
-[assembly: AssemblyFileVersion("1.0.12")]
+[assembly: AssemblyVersion("1.0.13")]
+[assembly: AssemblyFileVersion("1.0.13")]
