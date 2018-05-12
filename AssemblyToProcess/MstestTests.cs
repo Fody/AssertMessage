fd3bf7b838362172a02507db5f4a3131d8314dc3
Simon Cropp <simon.cropp@gmail.com>
2018-05-12 19:16:04 +1000

    .

--- AssemblyToProcess/MstestTests.cs
+++ AssemblyToProcess/MstestTests.cs
@@ -1,97 +1,94 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AssemblyToProcess
+public class MstestTests
 {
-    public class MstestTests
+    public void AreEqualInt_should_have_message()
     {
-        public void AreEqualInt_should_have_message()
-        {
-            var expected = 1;
-            var actual = 2;
+        var expected = 1;
+        var actual = 2;
 
-            // ReSharper disable once RedundantTypeArgumentsOfMethod
-            Assert.AreEqual<int>(expected, actual);
-        }
+        // ReSharper disable once RedundantTypeArgumentsOfMethod
+        Assert.AreEqual<int>(expected, actual);
+    }
 
-        public void AreEqual_should_have_message()
-        {
-            var expected = "1";
-            var actual = "2";
+    public void AreEqual_should_have_message()
+    {
+        var expected = "1";
+        var actual = "2";
 
-            Assert.AreEqual(expected, actual);
-        }
+        Assert.AreEqual(expected, actual);
+    }
 
-        public void StringContains_should_have_message()
-        {
-            var actual = "badstring";
+    public void StringContains_should_have_message()
+    {
+        var actual = "badstring";
 
-            StringAssert.Contains("test", actual);
-        }
+        StringAssert.Contains("test", actual);
+    }
 
-        public void AreEqual_should_have_message_original_formated_message()
-        {
-            var expected = 1;
-            var actual = 2;
+    public void AreEqual_should_have_message_original_formated_message()
+    {
+        var expected = 1;
+        var actual = 2;
 
-            Assert.AreEqual(expected, actual, "{0}_{1}", "original", "message");
-        }
+        Assert.AreEqual(expected, actual, "{0}_{1}", "original", "message");
+    }
 
-        public void AreEqual_should_have_message_for_object()
-        {
-            var expected = new object();
-            var actual = new object();
+    public void AreEqual_should_have_message_for_object()
+    {
+        var expected = new object();
+        var actual = new object();
 
-            Assert.AreEqual(expected, actual);
-        }
+        Assert.AreEqual(expected, actual);
+    }
 
-        public void AreEqual_should_have_message_original_message()
-        {
-            var expected = 1;
-            var actual = 2;
+    public void AreEqual_should_have_message_original_message()
+    {
+        var expected = 1;
+        var actual = 2;
 
-            Assert.AreEqual(expected, actual, "original_message");
-        }
+        Assert.AreEqual(expected, actual, "original_message");
+    }
 
-        public void AreNotEqual_should_have_message()
-        {
-            var notExpected = 1.5;
-            var actual = 1.5;
+    public void AreNotEqual_should_have_message()
+    {
+        var notExpected = 1.5;
+        var actual = 1.5;
 
-            Assert.AreNotEqual(notExpected, actual);
-        }
+        Assert.AreNotEqual(notExpected, actual);
+    }
 
-        public void Contains_should_have_message()
-        {
-            var expected = new object();
-            var collection = new[] { new object(), new object() };
+    public void Contains_should_have_message()
+    {
+        var expected = new object();
+        var collection = new[] {new object(), new object()};
 
-            CollectionAssert.Contains(collection, expected);
-        }
+        CollectionAssert.Contains(collection, expected);
+    }
 
-        public void IsTrue_should_have_message()
-        {
-            var actual = false;
+    public void IsTrue_should_have_message()
+    {
+        var actual = false;
 
-            Assert.IsTrue(actual);
-        }
+        Assert.IsTrue(actual);
+    }
 
-        public void IsFalse_should_have_message()
-        {
-            var actual = true;
+    public void IsFalse_should_have_message()
+    {
+        var actual = true;
 
-            Assert.IsFalse(actual);
-        }
+        Assert.IsFalse(actual);
+    }
 
-        public void IsInstanceOf_should_have_message()
-        {
-            var actual = new object();
+    public void IsInstanceOf_should_have_message()
+    {
+        var actual = new object();
 
-            Assert.IsInstanceOfType(actual, typeof(int));
-        }
+        Assert.IsInstanceOfType(actual, typeof(int));
+    }
 
-        public void Fail_should_have_message()
-        {
-            Assert.Fail();
-        }
+    public void Fail_should_have_message()
+    {
+        Assert.Fail();
     }
 }
--- Tests/MstestTests.cs
+++ Tests/MstestTests.cs
@@ -1,89 +1,86 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 
-namespace Tests
+[TestFixture]
+public class MstestTests : IntegrationTestsBase<AssertFailedException>
 {
-    [TestFixture]
-    public class MstestTests : IntegrationTestsBase<AssertFailedException>
+    [Test]
+    public void StringContains_should_have_message()
     {
-        [Test]
-        public void StringContains_should_have_message()
-        {
-            CheckIfMessageIsValid("StringAssert.Contains(\"test\", actual);");
-        }
+        CheckIfMessageIsValid("StringAssert.Contains(\"test\", actual);");
+    }
 
-        [Test]
-        public void AreEqual_should_have_message_original_formated_message()
+    [Test]
+    public void AreEqual_should_have_message_original_formated_message()
+    {
+        CheckIfMessageIsValid(message =>
         {
-            CheckIfMessageIsValid(message =>
-            {
-                NUnit.Framework.StringAssert.Contains("original_message", message);
-                NUnit.Framework.StringAssert.DoesNotContain("Assert.AreEqual(", message);
-            });       
-        }
+            NUnit.Framework.StringAssert.Contains("original_message", message);
+            NUnit.Framework.StringAssert.DoesNotContain("Assert.AreEqual(", message);
+        });
+    }
 
-        [Test]
-        public void AreEqual_should_have_message()
-        {
-            CheckIfMessageIsValid("Assert.AreEqual(expected, actual);");
-        }
+    [Test]
+    public void AreEqual_should_have_message()
+    {
+        CheckIfMessageIsValid("Assert.AreEqual(expected, actual);");
+    }
 
-        [Test]
-        public void AreEqualInt_should_have_message()
-        {
-            CheckIfMessageIsValid("Assert.AreEqual<int>(expected, actual);");
-        }
+    [Test]
+    public void AreEqualInt_should_have_message()
+    {
+        CheckIfMessageIsValid("Assert.AreEqual<int>(expected, actual);");
+    }
 
-        [Test]
-        public void AreEqual_should_have_message_for_object()
-        {
-            CheckIfMessageIsValid("Assert.AreEqual(expected, actual);");
-        }
+    [Test]
+    public void AreEqual_should_have_message_for_object()
+    {
+        CheckIfMessageIsValid("Assert.AreEqual(expected, actual);");
+    }
 
-        [Test]
-        public void AreEqual_should_have_message_original_message()
+    [Test]
+    public void AreEqual_should_have_message_original_message()
+    {
+        CheckIfMessageIsValid(message =>
         {
-            CheckIfMessageIsValid(message =>
-            {
-                NUnit.Framework.StringAssert.Contains("original_message", message);
-                NUnit.Framework.StringAssert.DoesNotContain("Assert.AreEqual(", message);
-            });       
-        }
+            NUnit.Framework.StringAssert.Contains("original_message", message);
+            NUnit.Framework.StringAssert.DoesNotContain("Assert.AreEqual(", message);
+        });
+    }
 
-        [Test]
-        public void AreNotEqual_should_have_message()
-        {
-            CheckIfMessageIsValid("Assert.AreNotEqual(notExpected, actual);");
-        }
+    [Test]
+    public void AreNotEqual_should_have_message()
+    {
+        CheckIfMessageIsValid("Assert.AreNotEqual(notExpected, actual);");
+    }
 
-        [Test]
-        public void Contains_should_have_message()
-        {
-            CheckIfMessageIsValid("CollectionAssert.Contains(collection, expected);");
-        }
+    [Test]
+    public void Contains_should_have_message()
+    {
+        CheckIfMessageIsValid("CollectionAssert.Contains(collection, expected);");
+    }
 
-        [Test]
-        public void IsTrue_should_have_message()
-        {
-            CheckIfMessageIsValid("Assert.IsTrue(actual);");
-        }
+    [Test]
+    public void IsTrue_should_have_message()
+    {
+        CheckIfMessageIsValid("Assert.IsTrue(actual);");
+    }
 
-        [Test]
-        public void IsFalse_should_have_message()
-        {
-            CheckIfMessageIsValid("Assert.IsFalse(actual);");
-        }
+    [Test]
+    public void IsFalse_should_have_message()
+    {
+        CheckIfMessageIsValid("Assert.IsFalse(actual);");
+    }
 
-        [Test]
-        public void IsInstanceOf_should_have_message()
-        {
-            CheckIfMessageIsValid("Assert.IsInstanceOfType(actual, typeof(int));");
-        }
+    [Test]
+    public void IsInstanceOf_should_have_message()
+    {
+        CheckIfMessageIsValid("Assert.IsInstanceOfType(actual, typeof(int));");
+    }
 
-        [Test]
-        public void Fail_should_have_message()
-        {
-            CheckIfMessageIsValid("Assert.Fail();");
-        }
+    [Test]
+    public void Fail_should_have_message()
+    {
+        CheckIfMessageIsValid("Assert.Fail();");
     }
 }
