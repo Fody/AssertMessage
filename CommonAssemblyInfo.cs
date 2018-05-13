c250b4cd08acfb9ba4bbfc123dda5d70a2ac6301
Simon Cropp <simon.cropp@gmail.com>
2018-05-13 14:40:52 +1000

    .

--- CommonAssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-[assembly: AssemblyTitle("Fody.AssertMessage")]
-[assembly: AssemblyProduct("Fody.AssertMessage")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
